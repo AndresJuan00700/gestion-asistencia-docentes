bfd38cc1a5da35275eaf676d4ae09d64601fe442
andres <benitezmunozandresjuan@gmail.com>
2024-10-29 12:29:05 -0500

    estilos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DocenteModule } from "./docente/docente.module";
@@ -18,6 +18,7 @@ import { SidebarModule } from './sidebar/sidebar.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+
     DocenteModule,
 
     LoginModule,
